aa9c35e76636c3ddab4e78778d0af7ea985c1c67
federico Lubbert <federico.lubbert@gmail.com>
2024-03-06 09:55:26 +0100

    init

--- main.py
+++ main.py
@@ -1,5 +1,102 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+import json
+
+FONT_NAME = "Courier"
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showwarning(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                # Creating new file
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=25, pady=45)
+
+# Labels
+website_label1 = Label(text="Website:", font=(FONT_NAME, 10, "bold"))
+website_label1.grid(column=0, row=1)
+email_label2 = Label(text="Email/Username:", font=(FONT_NAME, 10, "bold"))
+email_label2.grid(column=0, row=2)
+password_label3 = Label(text="Password:", font=(FONT_NAME, 10, "bold"))
+password_label3.grid(column=0, row=3)
+
+# Entries
+website_entry = Entry(width=52)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=52)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "example.email@gmail.com")
+password_entry = Entry(width=33)
+password_entry.grid(column=1, row=3)
+
+# Buttons
+generate_button = Button(text="Generate Password", command=generate_password)
+generate_button.grid(column=2, row=3)
+add_button = Button(width=44, text="Add", command=save)
+add_button.grid(column=1, row=4, columnspan=2)
+
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=0)
+
+window.mainloop()
